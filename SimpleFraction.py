1d1f47cc3dd75029e9e00caefa4257338be3c515
mngugi <mngugi7@gmail.com>
2019-06-08 22:34:03 +0300

    Add files via upload

---
+++ SimpleFraction.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+Author: Mwangi 
+a simple fraction example 
+ Fraction(numerator, denominator)
+"""
+
+from fractions import Fraction
+f = Fraction(5,10)
+print (f)
