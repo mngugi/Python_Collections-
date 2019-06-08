3a4c58704f6e63148b786a16860cee728825c15c
mngugi <mngugi7@gmail.com>
2019-06-08 23:23:03 +0300

    Add files via upload

---
+++ inputexample.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+Author: Mwangi 
+a simple user input function example 
+"""
+
+x = input()
+y = 'x is ineger'
+z = int(x) +1
+w = float(x) + 2.5
+
