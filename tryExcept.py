edd09e58ca73b35e3b73356a293d017adce9bba2
mngugi <mngugi7@gmail.com>
2019-06-08 23:41:21 +0300

    Add files via upload

---
+++ tryExcept.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+Author: Mwangi 
+a try ... except error handler 
+"""
+try:
+    x = int(input("Enter an integer number: "))
+    
+except ValueError:
+    print("Invalid Number! Try again ")
