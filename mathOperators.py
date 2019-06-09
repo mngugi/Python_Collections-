6317089c949981bc1b42ecaf0a1f00035bdb9307
mngugi <mngugi7@gmail.com>
2019-06-09 15:44:26 +0300

    Add files via upload

---
+++ mathOperators.py
@@ -0,0 +1,60 @@
+# Basic level mathematics using python
+# Author mwangi ngugi
+
+# Addition examples 
+print ( " ---- Addition a =  (1+2) and (ai = -1+2.5) ---- " )
+a = 1 + 2
+ai= -1 + 2.5
+print('Answers',  a , ',' , ai)
+
+#Subtraction examples 
+print ( "---- Subtraction b=(31-2) and bi=( -9-9) ----" )
+b = 31 - 2
+bi = -9-9
+print('Answers',  b, ',', bi)
+
+#Multiplication examples
+print ( "---- Multiply c=(4*20) and ci=( 2.5 * 6.5) ----" )
+c = 4*20
+ci = 2.5*6.5
+print('Answers',  c, ',', ci)
+
+#Division examples
+print(" ---- Division  d = ( 9/2.5 ) and di = (0.75/0.5) ---- ")
+d = ( 9/2.5 )
+di = (0.75/0.5)
+print('Answers', d , ',' , di)
+
+#Floor Division examples
+'''
+The floor division operator divides the first number by the second
+number and then rounds down the result to the next lowest integer. This
+becomes interesting when one of the numbers is negative.
+Reference Source : DOING MATH WITH PYTHON
+USE PROGRAMMING TO EXPLORE ALGEBRA ,STATISTICS , CALCULUS , AND MORE! page 3
+AMIT SAHA 
+'''
+print(" *** Floor Division  d = ( 9//2.5 ) and di = (0.75//0.5) ---- ")
+e = ( 9//2.5 )
+ei = (0.75//0.5)
+print('Answers', e , ',' , ei)
+
+# Remainder or Modulo (%) examples
+print(" ---- Remainder  f = ( 7%3 ) and fi = (0.8%0.6) ---- ")
+f = ( 7%3 )
+fi = (0.8%0.6)
+print('Aswers', f , ',' , fi)
+
+# Exponential examples
+print(" ---- Exponential   g = ( 5**4 ) and gi = ( 9 **(1/4)) ---- ")
+g = 5**4 
+gi = 9 **(1/4)
+print('Answers', g , ',' , gi)
+
+# Combinations PEDMAS or BODMAS
+print ( " ---- Mathematical Combinations h = 5**2 + 4/2 - 10  and hi = 24+6 - (12 -3) + 24**2 / 10  ---- " )
+h = 5**2 + 4/2 - 10 
+hi = 24+6 - (12 -3) + 24**2 / 10
+print('Answers', h , ',' , hi)             
+
+print ( " *************** End ****************" )
