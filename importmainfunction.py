f76e694f939dbb438062a84c47481e6f96b6c4a3
mngugi <mngugi7@gmail.com>
2019-06-10 17:59:57 +0300

    Add files via upload

---
+++ importmainfunction.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+Author : Mwangi
+Purpose : this program imports mainfunction 
+"""
+# note this will call print ('is this a main function example ?') first and 
+# print ("call mainfunction ") second
+import mainfunction
+print ("call mainfunction ")
+
+
+
---
+++ mainfunction.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+Author : Mwangi
+Purpose : a simple main example 
+"""
+print ('is this a main function example ?') #this code will be executed first 
+
+def main():
+    print('yes it is main method function') # then this will be excuted second 
+    
+# for the code to excute the main method then a condition is given i.e.,
+    
+if __name__ == '__main__':
+    main()
+    
+# the mainfunction can be imported in another program file i.e
+# import mainfunction
+# note this will call print ('is this a main function example ?')
+
