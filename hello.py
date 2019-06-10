3d4f6efa0e8f9f794f8811c4c989444fa15bb20a
mngugi <mngugi7@gmail.com>
2019-06-10 11:15:36 +0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+Author : Mwangi
+Purpose : Simple hello world function
+programming starts at function modules
+"""
+def Hello():
+    print ("Hello World Coders")
+    
+# call the function hello()    
+Hello() 
+
