2e9d82697d94bb08279b27f3287301540c79e74e
mngugi <mngugi7@gmail.com>
2019-06-10 12:31:28 +0300

    Add files via upload

---
+++ appendlist.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+Author : Mwangi
+Purpose : Simple program to append data types creating a lists
+
+"""
+empty = []
+print("1.Append an integer")
+print("-------------------")
+empty.append(21)
+print("2.Append a float")
+print("-------------------")
+empty.append(50.0889)
+print("3.Append a double")
+print("-------------------")
+empty.append(2.0)
+print("4.Append a string")
+print("-------------------")
+empty.append('Stingo')
+print("-------------------")
+print(empty)
+print("-------------------")
+
+
+
