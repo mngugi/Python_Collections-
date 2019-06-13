306e6b4f1d2600b473f62e6686c9f8a440c5ffbc
mngugi <mngugi7@gmail.com>
2019-06-13 21:26:20 +0300

    Add files via upload

---
+++ tutology.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+Author : Mwangi
+Purpose : a simple program to compute a simple truth and false 
+"""
+
+# print the OR logical operation 
+print('-------or----------')
+print (True or True)
+print('-----------------')
+print (True or False)
+print('-----------------')
+print (False or True)
+print('-----------------')
+print (False or False)
+print('-----------------')
+# print the ANd logical operation 
+print('-------And----------')
+print (True and True)
+print('-----------------')
+print (True and False)
+print('-----------------')
+print (False and True)
+print('-----------------')
+print (False and False)
+print('-----------------')
+
+
+
+
+
