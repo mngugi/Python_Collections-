bef167e80e8d5a4fc6a3e65be3175283f5825522
mngugi <mngugi7@gmail.com>
2019-06-17 15:54:18 +0300

    Add files via upload

---
+++ in.py
@@ -0,0 +1,21 @@
+
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+Author : Mwangi
+Purpose : the 'in ' and 'is' are keywords in python :
+    - in checks if a container contains a vlaue
+    - is used to test object identity
+"""
+if 'a' in 'Pandas':
+    print ("there are 2 a letters in the word pandas")
+else :print (" Panda is a python data science package")
+
+for i in 'Pandas':
+    print (i, end="")
+    
+    print ('\r')
+    
+    print ("is",''is'') # if a letter value is found and correct print is True
+    
+
