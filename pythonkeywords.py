d971a650bbd60a979eed849cfffb38e97eca581d
mngugi <mngugi7@gmail.com>
2019-06-20 23:05:07 +0300

    Add files via upload

---
+++ pythonkeywords.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+Author : Mwangi
+Purpose : a program to print pythons keywords 
+"""
+import keyword 
+print('Python Keywords are:')
+print (keyword.kwlist)
+
