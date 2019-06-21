67da61971064da9a3bf2d9a6ee1769590ce33942
mngugi <mngugi7@gmail.com>
2019-06-21 16:45:55 +0300

    Add files via upload

---
+++ inputs.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Author:Mwangi
+Purpose: A program for variable input
+    
+"""
+#input variables
+first_name = input("First Name: ")
+Middle_name = input("Middle Name: ")
+Last_name = input("First Name: ")
+age = input("Age : ")
+gender = input("Gender: ")
+title = input("Title: ")
+
+# print output
+print(title)
+print(first_name)
+print(Middle_name)
+print(Last_name)
+print(age, "years")
+print(gender)
