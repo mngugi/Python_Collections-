3147d67d3e942643fe46093bcccdc51d12ea2ddd
mngugi <mngugi7@gmail.com>
2022-09-20 13:27:46 +0300

    Update setup.py

--- PyDjuro/setup.py
+++ PyDjuro/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="arithmetic-YOUR-USERNAME-HERE", # Replace with your own username
+    name="arithmetic-mngugi", # Replace with your own username
     version="0.0.1",
     author="Peter Mwangi Ngugi",
     author_email="mngugi7@gmail.com",
