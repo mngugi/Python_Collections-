20b5d19c0cc746f5438762e18d52cfd3e539b9ee
mngugi <mngugi7@gmail.com>
2022-11-23 14:47:28 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -5,16 +5,16 @@ for computing a sentence and calculating profit
 """
 print(" ----- Example 1 ---- \n")
 def  simple():
-      print(" Hello , this a function in python ")
+      p = print(" this is a function simple()")
 # call the function 
-simple()      
+simple()   
 
 print(" ----- Example 2 ---- \n")
 
 def profit( ):
       buying_price  = 12000
       selling_price  = 15000
-      print ("Net profit is : " , selling_price - buying_price)
+      Net_Profit = selling_price - buying_price
+      print ("Net profit is : ", Net_Profit)
 profit()
-
       
