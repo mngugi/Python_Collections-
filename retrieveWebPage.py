4ba0dcbc534655ff97f931d4ae434a96f3cd8c39
M.ngugi <mngugi7@gmail.com>
2023-03-16 21:19:05 +0300

    Update retrieveWebPage.py

--- retrieveWebPage.py
+++ retrieveWebPage.py
@@ -1,6 +1,6 @@
 import urllib.request, urllib.parse, urllib.error
 
-fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+fhand = urllib.request.urlopen('http://<xxxxxx.com>/ffff.txt')
 
 counts = dict()
 for line in fhand:
