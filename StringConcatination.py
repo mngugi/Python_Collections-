0c853d9825e9cd346b7c7e03bd6e2525c3131905
M.ngugi <mngugi7@gmail.com>
2023-04-13 21:23:20 +0300

    Add files via upload

---
+++ StringConcatination.py
@@ -0,0 +1,14 @@
+userName = "erik.ten"
+domainName = "sentryinformation.co.ke"
+at = "@"
+userEmail = userName + at + domainName
+
+print(userEmail)
+
+print("Website:")
+
+website = "http://www.sentryinformation.com"
+param = "?p=369"
+url = "".join([website,param])
+
+print(url)
