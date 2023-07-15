4510bbe696eac2425ec095cd416441a4b107af9e
M.ngugi <mngugi7@gmail.com>
2023-07-15 23:08:01 +0300

    Add files via upload

---
+++ test_tensor.py
@@ -0,0 +1,15 @@
+
+
+
+import tensorflow as tf
+
+try:
+    from google.colab import drive
+    COLAB = True
+    print("Note: using Google Colab")
+
+except ModuleNotFoundError:
+    print("Note: not using Google Colab")
+    COLAB = False
+
+print("TensorFlow version:", tf.__version__)
