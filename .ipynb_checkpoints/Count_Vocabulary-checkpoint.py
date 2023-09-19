a9a4aae6556e63f80baad8ec5343001b80b1a8d5
M.ngugi <mngugi7@gmail.com>
2023-09-19 11:13:12 +0300

    Converted

---
+++ .ipynb_checkpoints/Count_Vocabulary-checkpoint.py
@@ -0,0 +1,8 @@
+#count the words in a text in a variety of useful ways
+# len is used to  length of a text from start to finish, in terms of the words
+#and punctuation symbols that appear.
+from nltk.book import *
+print ("the number of words in Genesis is :")
+l= len(text3)
+print(l)
+
