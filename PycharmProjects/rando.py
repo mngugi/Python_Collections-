dcc6ecf403b0c4be2e660edf0b35be61899ee392
M.ngugi <mngugi7@gmail.com>
2023-09-26 21:55:37 +0300

    rando.py

---
+++ PycharmProjects/rando.py
@@ -0,0 +1,12 @@
+import random
+
+HomexG = [0.21,0.66,0.1,0.14,0.01]
+AwayxG = [0.04,0.06,0.01,0.04,0.06,0.12,0.01,0.06]
+
+if random.random() <= 0.21:
+    print("GOAL!")
+
+else:
+    print("oh missed!")
+
+    
