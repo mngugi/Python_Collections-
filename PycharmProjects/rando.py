2b60e7eb2c6a06721ac159d9b0d7f748f000c736
M.ngugi <mngugi7@gmail.com>
2023-09-30 00:28:28 +0300

    Update Rando.py

--- PycharmProjects/rando.py
+++ PycharmProjects/rando.py
@@ -1,17 +1,29 @@
 import random
 
-HomexG = [0.21,0.66,0.1,0.14,0.01]
-AwayxG = [0.04,0.06,0.01,0.04,0.06,0.12,0.01,0.06]
+# Goal probabilities for home and away teams
+HomexG = [0.21, 0.66, 0.1, 0.14, 0.01]
+AwayxG = [0.04, 0.06, 0.01, 0.04, 0.06, 0.12, 0.01, 0.06]
 
-if random.random() <= 0.21:
-    print("GOAL!")
+# Function to simulate shots and calculate goals
+def shotsTest(shots):
+    goals = 0
 
-else:
-    print("oh missed!")
+    for shot in shots:
+        if random.random() <= shot:
+            goals += 1
+    return goals
 
-Goals = 0
-
+# Simulate 1000 matches
 for j in range(0, 1000):
-    if random.random() <=0.1:
-        Goals +=1
-print(Goals)
+    HomexG_copy = HomexG.copy()  # Make a copy of the original list
+    AwayxG_copy = AwayxG.copy()  # Make a copy of the original list
+
+    HomexG_goals = shotsTest(HomexG_copy)
+    AwayxG_goals = shotsTest(AwayxG_copy)
+
+    if HomexG_goals > AwayxG_goals:
+        print("Home Wins! {} - {}".format(HomexG_goals, AwayxG_goals))
+    elif HomexG_goals < AwayxG_goals:
+        print("Away Wins! {} - {}".format(HomexG_goals, AwayxG_goals))
+    else:
+        print("Share of the points! {} - {}".format(HomexG_goals, AwayxG_goals))
