7ff14a8e18064ca7ad7e6929ff2415fcf559afe2
M.ngugi <mngugi7@gmail.com>
2024-06-13 12:05:34 +0300

    Create Compute logic.py

---
+++ Compute logic.py
@@ -0,0 +1,10 @@
+a = '''
+
+Propositional logic is like the DNA of computer science,
+the building blocks that form the foundation of all things computational.
+By combining simple statements with what we call operators such as "and", "or" and "not,"
+you can create logic rules that help you reason about the truth or falsehood of a statement.
+
+'''
+
+print(a)
