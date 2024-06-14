d91a7d00b67e759330b132ab3d2a3e6289c5500c
M.ngugi <mngugi7@gmail.com>
2024-06-14 20:16:08 +0300

    Update Compute logic.py

--- Compute logic.py
+++ Compute logic.py
@@ -8,3 +8,54 @@ you can create logic rules that help you reason about the truth or falsehood of 
 '''
 
 print(a)
+
+
+b = '''
+
+Recap: Constructing a truth table
+ You can construct a truth table by completing the following steps.
+
+Identify the number of propositional variables involved in the compound proposition.
+
+Determine the number of rows required in the truth table based on the number of propositional variables. The number of rows is equal to 2 raised to the power of the number of propositional variables. For example, if there are two variables then there will be four rows (
+2
+2
+=
+2
+×
+2
+2 
+2
+ =2×2). If there are four variables then there will be 16 rows (
+2
+4
+=
+2
+×
+2
+×
+2
+×
+2
+2 
+4
+ =2×2×2×2).
+
+Create a column for each propositional variable involved in the expression or statement. Label each column with the corresponding variable.
+
+Add an additional column for the final output or truth value of the logical expression or statement.
+
+Fill in the rows of the truth table by systematically evaluating the truth values of each propositional variable for each row. Start by assigning the value 'true' (T) to the first half of the rows and 'false' (F) to the second half of the rows. Then, alternate the truth values for each variable in subsequent columns to complete the rows.
+
+Apply the logical operators (AND, OR, NOT, etc.) involved in the compound proposition to determine the final output or truth value for each row.
+
+Fill in the final output column with the resulting truth values of the logical expression or statement for each row.
+
+Review the truth table to ensure that it accurately represents the logical behaviour of the expression or statement.
+
+
+
+
+'''
+
+print(b)
