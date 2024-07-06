83b27872f056efe0f11ba7a6efe09f5eca2023db
M.ngugi <mngugi7@gmail.com>
2024-07-06 18:36:25 +0300

    Create forLoops.py

---
+++ forLoops.py
@@ -0,0 +1,13 @@
+for i in [1,4,6,9,7]:
+    print(i)
+
+for n in range(1):
+    print("=")
+
+for j in range(10):
+    print("what are you doing ", j)
+
+time = 0
+while time <= 5:
+    print(time)
+    time = time + 2
