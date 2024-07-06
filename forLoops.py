fcf862dec4405c9269c5ce5e59d1512b9cc63285
M.ngugi <mngugi7@gmail.com>
2024-07-06 18:42:48 +0300

    Update forLoops.py

--- forLoops.py
+++ forLoops.py
@@ -4,10 +4,22 @@ for i in [1,4,6,9,7]:
 for n in range(1):
     print("=")
 
-for j in range(10):
+for j in range(5):
     print("what are you doing ", j)
 
 time = 0
 while time <= 5:
     print(time)
     time = time + 2
+
+
+max_number = 5
+a = 1
+
+
+while a < max_number:
+    print("the maximum number is reached!")
+    a = a + 1
+
+print("programm exhausted")    
+    
