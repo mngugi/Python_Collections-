9451186dceaa6848753234c12dae0f84197f68e9
M.ngugi <mngugi7@gmail.com>
2024-07-08 21:43:57 +0300

    Create displayInvestgatioMessages.py

---
+++ displayInvestgatioMessages.py
@@ -0,0 +1,11 @@
+def investigationMessage():
+    print("Investgate Suspecious Activity")
+application_status = "potential concern"
+email_status = "OK!"
+
+if application_status == "potential concern":
+    print("application_log:")
+    investigationMessage()
+if email_status == "potential concern":
+    print("email log:")
+    dinvestigationMessage()
