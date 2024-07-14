15582a2a66d3d4d3e40b118b96e0ccd1d39384ec
M.ngugi <mngugi7@gmail.com>
2024-07-14 11:27:50 +0300

    Update refresh_Graphing.py

--- refresh_Graphing.py
+++ refresh_Graphing.py
@@ -1,26 +1,18 @@
-import seaborn as sns
-
 import matplotlib.pyplot as plt
 
-import pandas as pd
+# Create the data
+x_axis = [12, 15, 36, 65]
+y_axis = [15, 19, 40, 60]
 
+# Plot the data
+plt.plot(x_axis, y_axis, marker='o')
 
-dt = {
-
-    'x_axis': [12, 15, 36, 65, 49, 60],
-    'y_axis': [15, 19, 40, 60, 41, 59]
-
-
-
-    }
-
-dt = pd.Dataframe(data)
-
-sns.lineplot( X = 'x-axis', Y = 'y-axis', data = dt, marker='0')
-
+# Add titles and labels
 plt.title('Line using Line Graph')
 plt.xlabel('x-axis')
 plt.ylabel('y-axis')
 plt.grid(True)
+
+# Show the plot
 plt.show()
 
