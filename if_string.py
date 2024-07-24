e944dcffc7ab38fd1a0a79ea2a66868ead855b5a
M.ngugi <mngugi7@gmail.com>
2024-07-24 17:33:35 +0300

    Create if_string.py

---
+++ if_string.py
@@ -0,0 +1,8 @@
+user_name = {"Name" : "Maina", "Age": 42}
+name_check = "Maina"
+
+
+if name_check in user_name:
+    print(f'Found {name_check}')
+else:
+    print(f'Details not found {name_check}')
