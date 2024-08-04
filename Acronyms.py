ba42b535cd7f950cfe06f5e06a9dfd2e2080850b
M.ngugi <mngugi7@gmail.com>
2024-08-04 16:37:41 +0300

    Update Acronyms.py

--- Acronyms.py
+++ Acronyms.py
@@ -6,6 +6,9 @@ user_input = str(input("Enter a Phrase: ")) # user inputs a word
 text = user_input.split()
 Acronyms = " "
 for i in text:
-    Acronyms = Acronyms+str(i[2]).upper()
+    Acronyms = Acronyms+str(i[2]).upper() # the i or index position is 2 in the array
 
-print(Acronyms)
+for j in text:
+    Acronyms = Acronyms+str(j[0]).lower()
+
+print(Acronyms) # prints out the converted upper case character
