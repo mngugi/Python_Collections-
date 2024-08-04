635625d31be13528327247f71f1901e16637be58
M.ngugi <mngugi7@gmail.com>
2024-08-04 17:06:31 +0300

    .idea/misc.xml

--- add2Number.py
+++ add2Number.py
@@ -1,3 +1,13 @@
+
+user_input = input("Eneter Two Numbers: ")
+
 def add_TwoNumbers(i,j):
     return i + j
-print(add_TwoNumber(i = 56, j=92))
+
+# split the numbers into 2 parts
+numbers = user_input.split()
+num1 = int(numbers[0])
+num2 = int(numbers[1])
+
+
+print(add_TwoNumbers(i = num1, j=num2))
