dfa6b15a76a605eb8a02850d79b4a576a1f08fd9
M.ngugi <mngugi7@gmail.com>
2024-08-04 16:32:37 +0300

    Update Acronyms.py

--- Acronyms.py
+++ Acronyms.py
@@ -1,4 +1,8 @@
-user_input = str(input("Enter a Phrase: "))
+# this a python program that converts the
+# character in a string or word and makes
+# it an upper case.
+
+user_input = str(input("Enter a Phrase: ")) # user inputs a word
 text = user_input.split()
 Acronyms = " "
 for i in text:
