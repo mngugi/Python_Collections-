5ade5e6e69807e30656024d2a58a9dac79955c58
M.ngugi <mngugi7@gmail.com>
2024-08-04 16:27:17 +0300

    .idea/.gitignore

--- Acronyms.py
+++ Acronyms.py
@@ -2,6 +2,6 @@ user_input = str(input("Enter a Phrase: "))
 text = user_input.split()
 Acronyms = ""
 for i in text:
-    Acronyms = Acronyms+str(i[0]).upper()
+    Acronyms = Acronyms+str(i[1]).upper()
 
 print(Acronyms)
