d8fc400087a89d44df1217c1aefd8360b7ef7a12
M.ngugi <mngugi7@gmail.com>
2024-08-09 13:56:37 +0300

    Update waterMark.py

--- waterMark.py
+++ waterMark.py
@@ -7,7 +7,7 @@ y = [i**2 for i in x]
 plt.plot(x,y)
 
 plt.text(0.5,0.5, 'Stingo', alpha=0.3, fontsize=50, rotation=45,
-         ha='center' va='center', transform = plt.gca().transAxes)
+         ha='center', va='center', transform = plt.gca().transAxes)
 
 plt.show()
 
