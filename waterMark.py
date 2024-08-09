2ee3a38eff8d94dfe90b71ca2f9d14d1bf55dc2a
M.ngugi <mngugi7@gmail.com>
2024-08-09 13:55:53 +0300

    Create waterMark.py

---
+++ waterMark.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+x = range(10)
+y = [i**2 for i in x]
+
+
+plt.plot(x,y)
+
+plt.text(0.5,0.5, 'Stingo', alpha=0.3, fontsize=50, rotation=45,
+         ha='center' va='center', transform = plt.gca().transAxes)
+
+plt.show()
+
