05c66cb809f7b4cd29e5feb609c5eac18e2e5607
M.ngugi <mngugi7@gmail.com>
2024-08-13 11:29:40 +0300

    Update place_ValueOfNumbers.py

--- place_ValueOfNumbers.py
+++ place_ValueOfNumbers.py
@@ -4,16 +4,17 @@ def read_positions(number):
     tens = (number // 10) % 10
     hundreds = (number // 100) % 10
     thousands = (number // 1000) % 10
+    millions = (number // 1000000) % 10
 
     # Return the results
-    return thousands, hundreds, tens, ones
+    return millions,thousands, hundreds, tens, ones
 
 
 # Example usage:
 number = int(input("Enter Number: "))
 print("Number",number)
-thousands,hundreds, tens, ones = read_positions(number)
-print(f"thousand: {thousands}, Hundreds: {hundreds}, Tens: {tens}, Ones: {ones}")
+millions,thousands,hundreds, tens, ones = read_positions(number)
+print(f"million: {millions},thousand: {thousands}, Hundreds: {hundreds}, Tens: {tens}, Ones: {ones}")
 
 print('---------------------Round the numbers--------------------------')
 roundedNumber = round(number, -1) #rounds the number to the nearest 10tenth
