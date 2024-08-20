eda1eb1906182a1cdd9db0181f8702e43af433b0
M.ngugi <mngugi7@gmail.com>
2024-08-20 22:33:03 +0300

    Update buttonMsgBox_GUI.py

--- buttonMsgBox_GUI.py
+++ buttonMsgBox_GUI.py
@@ -11,9 +11,14 @@ def echoPhraseInMsgBox():
     return messagebox.showinfo("Button Pressed")
    
 
+def echoText():
+    return messagebox.showwarning("Type the correct command!")
 
-button = tk.Button(r, text='Print', width=25, command=echoPhraseInMsgBox)
+button = tk.Button(r, text='Print',background='blue',foreground='yellow', width=25, command=echoPhraseInMsgBox)
+
+label_1 = tk.Label(r, text="print", fg='white', bg='red', width=25, height=20, command=echoText )
 
 button.pack()
+label_1()
 r.mainloop()
 
