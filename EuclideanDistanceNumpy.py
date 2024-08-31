fe132bc165cf9c2823348d8f397e7a080f3bd0f0
M.ngugi <mngugi7@gmail.com>
2024-08-31 12:04:52 +0300

    Create EuclideanDistanceNumpy.py

---
+++ EuclideanDistanceNumpy.py
@@ -0,0 +1,13 @@
+import numpy
+
+
+
+def euclidean_Distance(pt1, pt2):
+    return np.linalg.norm(np.array(pt1) - np.array(pt2))  
+
+pt1 = (1, 2)
+pt2 = (4, 6)
+
+distance = euclidean_Distance(pt1, pt2)
+
+print(f"The Euclidean Distance between {pt1} and {pt2} is {distance:.2f}")
