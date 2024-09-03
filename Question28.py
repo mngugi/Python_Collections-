64744663bcfad93aff2e0db315ef480e1893a9f4
M.Ngugi <mngugi7@gmail.com>
2024-09-03 18:58:00 +0300

    Question28.py

--- Question28.py
+++ Question28.py
@@ -1,5 +1,6 @@
 num = 5
 
+
 if (num == 5):
     num += 1
     print("1")
@@ -9,3 +10,15 @@ if (num == 5):
 
 else:
     print("3")            
+
+
+print('---------------')
+
+i = 0
+while i < 5:
+    print(i, end= "")
+    i += 1
+    if i == 3:
+        break
+    else:
+        print(0,'\n')    
