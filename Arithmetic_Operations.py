3c31a58207c26c791d324841740fa1337c25339e
M.Ngugi <mngugi7@gmail.com>
2024-09-03 17:18:21 +0300

    Update Arithmetic_Operations.py

--- Arithmetic_Operations.py
+++ Arithmetic_Operations.py
@@ -1,3 +1,19 @@
+---
+jupyter:
+  jupytext:
+    cell_metadata_filter: -all
+    custom_cell_magics: kql
+    text_representation:
+      extension: .py
+      format_name: markdown
+      format_version: '1.3'
+      jupytext_version: 1.11.2
+  kernelspec:
+    display_name: .venv
+    language: python
+    name: python3
+---
+
 import numpy as np
 # create an array of 5 integers
 arr = np.arange(5)
