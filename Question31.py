aa8eba9176b90b12294514324a6f31128a70f338
M.Ngugi <mngugi7@gmail.com>
2024-09-10 00:15:39 +0300

    Update Question31.py

--- Question31.py
+++ Question31.py
@@ -10,3 +10,15 @@ toyota: Car = Car('White', 184)
 print(toyota.color)
 print(toyota.horsepower)
 
+'''
+def display_info(self):
+    print(f"Car color: {self.color}")
+    print(f"Car max speed: {self.max_speed} km/h")
+
+
+# Now, create an instance of the Car class with the required arguments
+toyota: Car = Car('White', 184)
+
+# Optionally, use a method to display the car's information
+toyota.display_info()
+'''
