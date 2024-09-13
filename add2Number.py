fd738ddfa958f429749972b6e286c2b9f8e5aedb
M.Ngugi <mngugi7@gmail.com>
2024-09-13 17:55:47 +0300

    Update add2Number.py

--- add2Number.py
+++ add2Number.py
@@ -1,13 +1,37 @@
 
 '''
-Spyder Editor
-Author : Mwangi
-Purpose : Enter two numbers and per addition using a
-add_TwoNumbers() function.
+# Script Name: add_two_numbers.py
+# Author: Mwangi
+# Purpose: This script prompts the user to enter two numbers, then performs addition
+#          using the add_TwoNumbers() function, and prints the result.
+# Usage: The user is expected to input two space-separated integers when prompted.
+#        The script will then display the sum of the two numbers.
+#
+# Function: add_TwoNumbers(i, j)
+#   - Description: Takes two integers and returns their sum.
+#   - Parameters:
+#       - i (int): The first integer.
+#       - j (int): The second integer.
+#   - Returns:
+#       - int: The sum of the two integers.
+#
+# Example:
+#   Input: 5 10
+#   Output: 15
+
 '''
 user_input = input("Enter Two Numbers: ") # input method.
 
 def add_TwoNumbers(i,j): # creating a add_TwoNumbers() function
+
+    """
+       Function to add two numbers.
+       Args:
+           i (int): First number
+           j (int): Second number
+       Returns:
+           int: Sum of the two numbers
+       """
     return i + j
 
 # split the numbers into 2 parts
