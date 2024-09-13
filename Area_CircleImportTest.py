ab29c133c3f43407f89a83901c0850435dc856e5
M.Ngugi <mngugi7@gmail.com>
2024-09-13 21:35:00 +0300

    Area_CircleImportTest.py

--- Area_CircleImportTest.py
+++ Area_CircleImportTest.py
@@ -1,7 +1,20 @@
+
+# Script Name: Area_CircleImportTest.py
+# Author: Mwangi
+# Purpose: This script Calculates the area of a circle.
+# The program tests Area_Circle module
+# Usage: For example the expected outcome is :
+#
+# Area of the circle with radius 3: 28.27 square meters
+# Surface area of the cylinder with radius 3 and height 5: 150.80 square meters
+#
+
 # Import the module Area_Circle
+
 import Area_Circle
 
 # Use the functions defined in Area_Circle
+
 radius = 3
 height = 5
 
