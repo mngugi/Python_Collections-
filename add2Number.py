1a7b9a71f078338e22b1a7660ee85e39abc285b4
M.Ngugi <mngugi7@gmail.com>
2024-09-13 17:53:22 +0300

    Update add2Number.py

--- add2Number.py
+++ add2Number.py
@@ -5,15 +5,15 @@ Author : Mwangi
 Purpose : Enter two numbers and per addition using a
 add_TwoNumbers() function.
 '''
-user_input = input("Enter Two Numbers: ")
+user_input = input("Enter Two Numbers: ") # input method.
 
-def add_TwoNumbers(i,j):
+def add_TwoNumbers(i,j): # creating a add_TwoNumbers() function
     return i + j
 
 # split the numbers into 2 parts
 numbers = user_input.split()
-num1 = int(numbers[0])
-num2 = int(numbers[1])
+num1 = int(numbers[0]) # numbers at the first position
+num2 = int(numbers[1]) # numbers at the second position
 
 
-print(add_TwoNumbers(i = num1, j=num2))
+print(add_TwoNumbers(i = num1, j=num2)) # print tht results
