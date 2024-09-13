0740173bc316821ae76849dc9392fd081e17e8d5
M.Ngugi <mngugi7@gmail.com>
2024-09-13 22:09:45 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,3 +1,12 @@
+
+# Script Name: calculator.py
+# Author: Mwangi
+# Purpose: This script prompts the user to enter two numbers, then performs addition
+#          using the add_TwoNumbers() function, and prints the result.
+# Usage: The user is expected to input two space-separated integers when prompted.
+#        The script will then display the sum of the two numbers.
+
+
 def add(*numbers: float) -> float:
     return sum(numbers)
 
