71659e9621a1f2c5f93c59a8c1eab6a2f52523ab
M.Ngugi <mngugi7@gmail.com>
2024-09-13 22:24:25 +0300

    CalculateSum.py

---
+++ CalculateSum.py
@@ -0,0 +1,12 @@
+
+# Script Name: CalculateSum.py
+# Author: Mwangi
+# Purpose: This script using the add() function from calculator.py module, and prints the result.
+# Usage: the program is also a module for the subsequent programm CalculateSum.py
+#
+
+# import module cslculator
+
+import calculator
+
+print(calculator.add(10,12,52,68,37))
--- calculator.py
+++ calculator.py
@@ -1,13 +1,16 @@
 
 # Script Name: calculator.py
 # Author: Mwangi
-# Purpose: This script prompts the user to enter two numbers, then performs addition
-#          using the add_TwoNumbers() function, and prints the result.
-# Usage: The user is expected to input two space-separated integers when prompted.
-#        The script will then display the sum of the two numbers.
+# Purpose: This script prompts the user to enter many numbers, then performs addition or sum
+#          using the add() function, and prints the result.
+# Usage: the program is also a module for the subsequent programm CalculateSum.py
+#
+#
 
 
 def add(*numbers: float) -> float:
     return sum(numbers)
 
-print(add(52,63,98))    
+    if __name__ == ' __main__ ':
+
+        print(add(52,63,98))    
