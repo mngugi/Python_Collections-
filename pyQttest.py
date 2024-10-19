73fa95a5b68e2efc719e0a5c8d87544f9b2a0c62
mngugi <mngugi7@gmail.com>
2024-10-19 22:54:32 +0300

    .idea/Python_Collections-.iml

--- pyQttest.py
+++ pyQttest.py
@@ -1,26 +1,30 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel # type: ignore
-import warnings
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
 
-warnings.filterwarnings("ignore")
-def main():
-   # Create the application instance
+def window():
    app = QApplication(sys.argv)
+   win = QDialog()
+   b1 = QPushButton(win)
+   b1.setText("Button1")
+   b1.move(50,20)
+   b1.clicked.connect(b1_clicked)
 
-   # Create the main window
-   window = QMainWindow()
-   window.setWindowTitle("Simple PyQt Example")
-   window.setGeometry(100, 100, 400, 200)
+   b2 = QPushButton(win)
+   b2.setText("Button2")
+   b2.move(50,50)
+   QObject.connect(b2,SIGNAL("clicked()"),b2_clicked)
 
-   # Create a label widget
-   label = QLabel("Hello, PyQt!", window)
-   label.move(150, 80)
+   win.setGeometry(100,100,200,100)
+   win.setWindowTitle("PyQt")
+   win.show()
+   sys.exit(app.exec_())
 
-   # Show the window
-   window.show()
+def b1_clicked():
+   print ("Button 1 clicked")
 
-   # Execute the application
-   sys.exit(app.exec())
+def b2_clicked():
+   print ("Button 2 clicked")
 
-if __name__ == "__main__":
-   main()
+if __name__ == '__main__':
+   window()
