074e5b88fd0c9aa932067ae5dc59c52eb5f566cb
M.Ngugi <mngugi7@gmail.com>
2024-11-21 19:05:03 +0300

    Update underlyingMechansim.py

--- underlyingMechansim.py
+++ underlyingMechansim.py
@@ -1,7 +1,14 @@
  # x is assigned to the value 12345
  # y is assigned to the value 12345
+ # to assign objects in memory
 x = 12345
 y = 12345
 
 id(x)
 id(y)
+
+# comples number has two parts an iaginary number and a real number.
+# for example
+
+c = 1j
+# call type(c)
