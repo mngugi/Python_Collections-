b4b3fe54d8bbccbcb8379e57c8f55581ca619639
M.Ngugi <mngugi7@gmail.com>
2024-11-30 23:05:59 +0300

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,7 +1,7 @@
-print('I am' +  str(25)  + 'years.')
+print('I am ' +  str(25)  + ' years.')
 
 
 print('What is your age pal?')
 your_age = input()
-print('My age is' + int(your_age) + 'thank you!')
+print('My age is ' + str(int(your_age))  + ' thank you!')
 
