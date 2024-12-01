604e0cb30a1350967fbe2fb12a2f3678d4a1e785
M.Ngugi <mngugi7@gmail.com>
2024-12-01 22:12:31 +0300

    Update Car_Small.py

--- Car_Small.py
+++ Car_Small.py
@@ -1,4 +1,15 @@
+# Script Name: Area_Circle.py
+# Author: Mwangi
+# Purpose: This snippet creates a class called Car.
+# Usage: For example the expected outcome is :
+#
+# 
+# 
+# 
+#
+# 
 # a class called car.
+
 class Car:
     def printSize(self):
         print("small")
