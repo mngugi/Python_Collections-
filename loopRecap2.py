ddaf59486d35e898cc1dea7a5ec2c0e246e63a1c
M.Ngugi <mngugi7@gmail.com>
2024-12-07 12:40:55 +0300

    Update loopRecap2.py

--- loopRecap2.py
+++ loopRecap2.py
@@ -1,8 +1,9 @@
 count = 0
+count2 = 0
 while count < 12 :
     print("Key Number is: ", count)
     count +=1
 
     print("---------\n")
     print("Key Number of 2: ")
-    count +=2
+    count2 +=2
