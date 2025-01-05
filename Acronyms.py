1517ba2151bf7138f924202e090eb8fa33650446
M.Ngugi <mngugi7@gmail.com>
2025-01-05 22:25:43 +0300

    Update Acronyms.py

--- Acronyms.py
+++ Acronyms.py
@@ -1,5 +1,22 @@
 # Your updated Python code here
 # Python Acronyms Dictionary
+'''
+# Script Name: add_two_numbers.py
+# Author: Mwangi
+# Purpose: This program print pythons acronyms 
+# Example:
+#PEP: Python Enhancement Proposal
+GIL: Global Interpreter Lock
+CPython: C implementation of Python
+IDLE: Integrated Development and Learning Environment
+PyPI: Python Package Index
+Pandas: Panel Data
+SciPy: Scientific Python
+Django: Named after Django Reinhardt, jazz guitarist
+Flask: A lightweight web framework in Python
+JSON: JavaScript Object Notation
+
+'''
 acronyms = {
     "PEP": "Python Enhancement Proposal",
     "GIL": "Global Interpreter Lock",
