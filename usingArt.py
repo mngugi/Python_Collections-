52baf597cecdf89fcbf91522351211dbb2e4054c
M.Ngugi <mngugi7@gmail.com>
2025-01-08 21:35:01 +0300

    Update usingArt.py

--- usingArt.py
+++ usingArt.py
@@ -4,5 +4,17 @@ def display_title(title):
     ascii_art = pyfiglet.figlet_format(title)
     print(ascii_art)
 
+def display_title(title):
+    border = "*~" * (len(title) // 2 + 1)
+    print(border)
+    print(f"   {title}")
+    print(border)    
+
 if __name__ == "__main__":
     display_title("Fancy Title")
+
+
+
+if __name__ == "__main__":
+    display_title("My Fancy Title")
+
