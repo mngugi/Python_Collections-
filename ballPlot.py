1e474c5fd1002b6c10d819d957cdde6a02a3dc07
M.Ngugi <mngugi7@gmail.com>
2025-01-09 22:29:10 +0300

    Update ballPlot.py

--- ballPlot.py
+++ ballPlot.py
@@ -1,6 +1,14 @@
 from numpy import linspace
 
 import matplotlib.pyplot as plt
+import pyfiglet # type: ignore
+
+def display_title(title):
+    ascii_art = pyfiglet.figlet_format(title)
+    print(ascii_art)
+
+if __name__ == "__main__":
+    display_title("Ball Plot")
 
 velocity = 5
 
