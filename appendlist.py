778e4624631ce9eab9a20726c1d857fd1a4cd403
M.Ngugi <mngugi7@gmail.com>
2025-01-09 22:08:39 +0300

    Update appendlist.py

--- appendlist.py
+++ appendlist.py
@@ -1,4 +1,12 @@
 # -*- coding: utf-8 -*-
+import pyfiglet # type: ignore
+
+def display_title(title):
+    ascii_art = pyfiglet.figlet_format(title)
+    print(ascii_art)
+
+if __name__ == "__main__":
+    display_title("Append List")
 """
 Spyder Editor
 Author : Mwangi
