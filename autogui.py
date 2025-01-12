bb8330e972a584ca3a4fe6f708731328e9d6c618
M.Ngugi <mngugi7@gmail.com>
2025-01-12 22:38:10 +0300

    Update autogui.py

--- autogui.py
+++ autogui.py
@@ -1,3 +1,10 @@
 import pyautogui
+import pyfiglet # type: ignore
 
+def display_title(title):
+    ascii_art = pyfiglet.figlet_format(title)
+    print(ascii_art)
+
+if __name__ == "__main__":
+    display_title("PyAuto Gui")
 
