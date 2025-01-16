61bba344c00b7a1c778ec5f8d192be2ed40f19a6
M.Ngugi <mngugi7@gmail.com>
2025-01-16 20:52:12 +0300

    auto_library.py

--- .ipynb_checkpoints/auto_library-checkpoint.py
+++ .ipynb_checkpoints/auto_library-checkpoint.py
@@ -1,4 +1,12 @@
 import pyautogui
+import pyfiglet # type: ignore
+
+def display_title(title):
+    ascii_art = pyfiglet.figlet_format(title)
+    print(ascii_art)
+
+if __name__ == "__main__":
+    display_title("Python Acronyms")
 
 pyautogui.scroll(60)
 
@@ -7,7 +15,5 @@ pyautogui.moveTo(100,100)
 
 pyautogui.click(clicks=3, interval=1)
 
-
-SVDSVD
-
+#SVDSVD
 
--- auto_library.py
+++ auto_library.py
@@ -1,4 +1,12 @@
 import pyautogui
+import pyfiglet # type: ignore
+
+def display_title(title):
+    ascii_art = pyfiglet.figlet_format(title)
+    print(ascii_art)
+
+if __name__ == "__main__":
+    display_title("Python Acronyms")
 
 pyautogui.scroll(60)
 
