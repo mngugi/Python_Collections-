dacd476be4c63150ff7b2d4d297dcbb62f9e43bb
M.Ngugi <mngugi7@gmail.com>
2025-01-16 20:56:43 +0300

    ImportScript.ipynb

---
+++ .ipynb_checkpoints/appendlist-checkpoint.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+import pyfiglet # type: ignore
+
+def display_title(title):
+    ascii_art = pyfiglet.figlet_format(title)
+    print(ascii_art)
+
+if __name__ == "__main__":
+    display_title("Append List")
+"""
+Spyder Editor
+Author : Mwangi
+Purpose : Simple program to append data types creating a lists
+
+[]: This is the literal syntax for creating an empty list in Python.
+empty = []: This assigns the empty list to the variable empty.
+For example the expected outcome is :
+1. Append an integer
+-------------------
+2. Append a float
+-------------------
+3. Append a double (float in Python)
+-------------------
+4. Append a string
+-------------------
+-------------------
+[21, 50.0889, 2.0, 'Stingo', 'Stingo is a funny name!', ':0 , :))']
+-------------------
+"""
+from typing import List, Union
+
+# Define a list that can hold integers, floats, and strings
+empty: List[Union[int, float, str]] = []
+
+print("1. Append an integer")
+print("-------------------")
+empty.append(21)
+
+print("2. Append a float")
+print("-------------------")
+empty.append(50.0889)
+
+print("3. Append a double (float in Python)")
+print("-------------------")
+empty.append(2.0)
+
+print("4. Append a string")
+print("-------------------")
+empty.append('Stingo')
+print("-------------------")
+empty.append("Stingo is a funny name!")
+empty.append(":0 , :))")
+
+print(empty)
+print("-------------------")
