ebfcaf2cd9e7d9c523d190a28a6df264133b5851
M.Ngugi <mngugi7@gmail.com>
2025-01-16 22:50:44 +0300

    Update brownWords.py

--- brownWords.py
+++ brownWords.py
@@ -1,2 +1,10 @@
 from nltk.corpus import brown 
+import pyfiglet # type: ignore
+
+def display_title(title):
+    ascii_art = pyfiglet.figlet_format(title)
+    print(ascii_art)
+
+if __name__ == "__main__":
+    display_title("Brown Words ")
 brown.words()
