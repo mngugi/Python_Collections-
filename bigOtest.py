5ea2aaa6a51610b71bf82bffc122bdf87062f19f
mngugi <mngugi7@gmail.com>
2025-03-28 23:00:35 +0300

    Update bigOtest.py

--- bigOtest.py
+++ bigOtest.py
@@ -1,4 +1,7 @@
-n = [2,3,6,8,9]
+n = [2, 3, 6, 8, 9]
+m = [5, 6, 8, 7, 8]
 
-for i in n:
-    print (i)
+for i in range(len(n)):      # Iterates over the indices of n
+    for j in range(len(m)):  # Iterates over the indices of m
+        print(n[i], m[j])    # Prints corresponding elements
+
