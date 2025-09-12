ddf23cc9b2af4a3cbe92bfe10f4c3f640eeb7f5e
mngugi <mngugi7@hotmail.com>
2025-09-13 00:51:15 +0300

    Colorspyfiglet.py

--- Colorspyfiglet.py
+++ Colorspyfiglet.py
@@ -1,10 +1,16 @@
+
+#Available Colors
+
+#With termcolor, you can use:
+
+#grey, red, green, yellow, blue, magenta, cyan, white
 from pyfiglet import Figlet
 from termcolor import colored
 from colorama import init
 
 # Initialize colorama for Windows compatibility (does nothing on Linux/macOS)
 init()
-
+#
 # Create pyfiglet text
 f = Figlet(font='slant')
 ascii_art = f.renderText('Hello!')
