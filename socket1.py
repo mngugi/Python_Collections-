0be4ed8c26ed4aed92b5c1b29520ef4eae5f6df6
mngugi <mngugi7@hotmail.com>
2025-11-26 22:22:17 +0300

    socket1.py

--- socket1.py
+++ socket1.py
@@ -14,6 +14,3 @@ client.sendall(b'GET /HTTP/1.1\r\n\r\n')
 # receive some data
 response = client.recv(4096)
 print (response)
-+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-b'HTTP/1.1 400 Bad Request\r\nDate: Tue, 11 Sep 2018 12:12:29 GMT\r\nServer: Apache/2.4.7 (Ubuntu)\r\nContent-Length: 437\r\nConnection: close\r\nContent-Type: text/html; charset=iso-8859-1\r\n\r\n<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">\n<html><head>\n<title>400 Bad Request</title>\n</head><body>\n<h1>Bad Request</h1>\n<p>Your browser sent a request that this server could not understand.<br />\nReason: You\'re speaking plain HTTP to an SSL-enabled server port.<br />\n Instead use the HTTPS scheme to access this URL, please.<br />\n</p>\n<hr>\n<address>Apache/2.4.7 (Ubuntu) Server at 127.0.1.1 Port 443</address>\n</body></html>\n'
-+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
